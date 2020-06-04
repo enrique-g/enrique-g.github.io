bbee16438b1d453882f72c9f750490650169fadb
enrique_g <enrique.gonzalez.de.vallejo@gmail.com>
2020-06-04 20:16:25 +0200

    form 1.1

--- formulario.php
+++ formulario.php
@@ -8,5 +8,10 @@ $mensaje = $_POST("mesage");
 $msg = "nombre:" . $nombre ." email:" . $email. "mensaje:". $mensaje."";
 
 
-mail ( "enrique.gonzalez.de.vallejo@gmail.com" ,  $asunto ,  $msg);
+$send = mail ( "enrique.gonzalez.de.vallejo@gmail.com" ,  $asunto ,  $msg);
+if($send){
+    echo "Email enviado";
+} else{
+    echo "Error al enviar el email";
+}
 header("Location: https://enrique-g.github.io");
